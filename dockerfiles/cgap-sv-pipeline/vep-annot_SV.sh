2ecc2cc46a196dcb524a6b3c2c19c13fc08bff8e
phil-grayson <ubuntu@ip-172-31-95-19.ec2.internal>
2021-05-18 19:03:06 +0000

    added vep-annot_SV, mod to dockerfile

---
+++ dockerfiles/cgap-sv-pipeline/vep-annot_SV.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# variables from command line
+input_vcf='sorted_manta.vcf.gz'
+reference='/mnt/mydata/GAPFIXRDPDK5.fa'
+
+# data sources
+vep_tar_gz='/mnt/mydata/GAPFIPK4VGWV.vep.tar.gz'
+version='101'
+assembly='GRCh38'
+
+# self variables
+directory=VCFS/
+
+tar -xzf $vep_tar_gz
+tar -xzf ${vep_tar_gz%%.*}.plugins.tar.gz
+tar -xzf $fordownload_tar_gz
+
+# setting up output directory
+mkdir -p $directory
+
+# command line VEP
+
+options="--fasta $reference --assembly $assembly --use_given_ref --offline --overlaps --max_sv_size 250000000 --cache_version $version --dir_cache . --force_overwrite --vcf"
+
+command="vep -i $input_vcf -o ${directory}test.vep.vcf $options"
+
+echo "Running VEP"
+$command
+
+cp ${directory}test.vep.vcf sv_annotated_vep.vcf
+
+bgzip sv_annotated_vep.vcf || exit 1
+tabix -p vcf sv_annotated_vep.vcf.gz || exit 1
